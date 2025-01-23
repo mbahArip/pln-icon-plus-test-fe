4961938416cf9a752d14967ed9c62199fe485af5
mbaharip <62494292+mbahArip@users.noreply.github.com>
2025-01-23 17:15:31 +0700

    Add loading and error state when fetching

--- src/views/Form/Add/index.tsx
+++ src/views/Form/Add/index.tsx
@@ -1,6 +1,7 @@
 import { Button, buttonVariants } from "@/components/Button";
 import Heading from "@/components/form/Heading";
 import { Checkbox, Input, InputWrapper, Select } from "@/components/form/Inputs";
+import { Icon } from "@/components/Icon";
 import {
   FormInputs,
   FormInputsKey,
@@ -13,7 +14,7 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 import { Link } from "react-router";
 
 export default function ViewFormAdd() {
-  const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string>("");
   const [formError, setFormError] = useState<Record<FormInputsKey, string>>({
     unit: "",
@@ -37,7 +38,6 @@ export default function ViewFormAdd() {
     consumption: {},
     consumptionPrice: 0,
   });
-  const [submitAllowed, setSubmitAllowed] = useState<boolean>(false);
 
   const [unitOptions, setUnitOptions] = useState<FormMasterOffice[]>([]);
   const [roomData, setRoomData] = useState<FormMasterMeetingRoom[]>([]);
@@ -112,27 +112,6 @@ export default function ViewFormAdd() {
 
     setValues((prev) => ({ ...prev, consumption: data, consumptionPrice: basePrice * values.participant }));
   }, [values.timeStart, values.timeEnd, values.participant, consumptionData]);
-
-  useEffect(() => {
-    if (Object.values(formError).some((error) => error !== "")) {
-      setSubmitAllowed(false);
-      return;
-    }
-
-    if (
-      values.unit === "" ||
-      values.room === "" ||
-      values.date === "" ||
-      values.timeStart === 0 ||
-      values.timeEnd === 0 ||
-      values.participant === 0
-    ) {
-      setSubmitAllowed(false);
-      return;
-    }
-
-    setSubmitAllowed(true);
-  }, [values, formError]);
 
   const onUnitChange = useCallback(
     (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -237,155 +216,179 @@ export default function ViewFormAdd() {
         showBackButton
       />
 
-      <div
-        className="px-7 py-5 flex flex-col gap-9 bg-white border border-form-border rounded-lg text-form-foreground"
-        style={{
-          boxShadow: "0px 4px 20px #6A6A6A1A",
-        }}
-      >
-        <div className="flex flex-col gap-4">
-          <h3 className="font-semibold text-base leading-[21.79px]">Informasi Ruang Meeting</h3>
-          <div className="flex items-start gap-5">
-            <InputWrapper label="Unit" labelProps={{ htmlFor: "unit" }} error={formError.unit}>
-              <Select
-                id="unit"
-                options={unitOptions.map((unit) => ({
-                  label: unit.officeName,
-                  value: unit.id,
-                }))}
-                value={values.unit}
-                placeholder="Pilih Unit"
-                onChange={onUnitChange}
-              />
-            </InputWrapper>
-            <InputWrapper label="Ruang Meeting" labelProps={{ htmlFor: "room" }} error={formError.room}>
-              <Select
-                id="room"
-                options={roomOptions.map((room) => ({
-                  label: room.roomName,
-                  value: room.id,
-                }))}
-                value={values.room}
-                placeholder="Pilih Ruang Meeting"
-                onChange={onRoomChange}
-              />
-            </InputWrapper>
+      {loading ? (
+        <div className="grid min-h-[66dvh] place-items-center">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Icon name="LoaderCircle" className="size-10 stroke-form-primary animate-spin" />
+            <p className="text-[12px] text-form-foreground opacity-80 animate-pulse">Sedang memuat data...</p>
+          </div>
+        </div>
+      ) : error ? (
+        <div className="grid min-h-[66dvh] place-items-center">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Icon name="Frown" className="size-10 stroke-red-500" />
+            <p className="text-[12px] text-red-500">{error ?? "Unknown error occured"}</p>
+          </div>
+        </div>
+      ) : (
+        <div
+          className="px-7 py-5 flex flex-col gap-9 bg-white border border-form-border rounded-lg text-form-foreground"
+          style={{
+            boxShadow: "0px 4px 20px #6A6A6A1A",
+          }}
+        >
+          <div className="flex flex-col gap-4">
+            <h3 className="font-semibold text-base leading-[21.79px]">Informasi Ruang Meeting</h3>
+            <div className="flex items-start gap-5">
+              <InputWrapper label="Unit" labelProps={{ htmlFor: "unit" }} error={formError.unit}>
+                <Select
+                  id="unit"
+                  options={unitOptions.map((unit) => ({
+                    label: unit.officeName,
+                    value: unit.id,
+                  }))}
+                  value={values.unit}
+                  placeholder="Pilih Unit"
+                  onChange={onUnitChange}
+                />
+              </InputWrapper>
+              <InputWrapper label="Ruang Meeting" labelProps={{ htmlFor: "room" }} error={formError.room}>
+                <Select
+                  id="room"
+                  options={roomOptions.map((room) => ({
+                    label: room.roomName,
+                    value: room.id,
+                  }))}
+                  value={values.room}
+                  placeholder="Pilih Ruang Meeting"
+                  onChange={onRoomChange}
+                />
+              </InputWrapper>
+            </div>
+
+            <div className="flex items-start gap-5">
+              <InputWrapper
+                label="Kapasitas"
+                labelProps={{
+                  htmlFor: "kapasitas",
+                }}
+                error={formError.capacity}
+              >
+                <Input
+                  type="number"
+                  id="kapasitas"
+                  placeholder="Masukkan kapasitas ruangan"
+                  disabled
+                  value={values.capacity}
+                  onChange={(e) => setValues((prev) => ({ ...prev, capacity: Number(e.target.value) }))}
+                />
+              </InputWrapper>
+            </div>
           </div>
 
-          <div className="flex items-start gap-5">
-            <InputWrapper
-              label="Kapasitas"
-              labelProps={{
-                htmlFor: "kapasitas",
+          <hr className="border-t-2 border-form-border" />
+
+          <div className="flex flex-col gap-5">
+            <h3 className="font-semibold text-base leading-[21.79px]">Informasi Rapat</h3>
+            <div className="flex items-start gap-5">
+              <InputWrapper label="Tanggal Rapat" labelProps={{ htmlFor: "date" }} error={formError.date}>
+                <Input
+                  type="date"
+                  id="date"
+                  placeholder="Pilih tanggal rapat"
+                  required
+                  value={values.date}
+                  onChange={onDateChange}
+                />
+              </InputWrapper>
+              <InputWrapper label="Waktu Mulai" labelProps={{ htmlFor: "timeStart" }} error={formError.timeStart}>
+                <Select
+                  id="timeStart"
+                  disabled={values.date === ""}
+                  options={timeOptions}
+                  value={values.timeStart === 0 ? "" : values.timeStart.toString()}
+                  placeholder="Pilih Waktu Mulai"
+                  onChange={onTimeStartChange}
+                />
+              </InputWrapper>
+              <InputWrapper label="Waktu Selesai" labelProps={{ htmlFor: "timeEnd" }} error={formError.timeEnd}>
+                <Select
+                  id="timeEnd"
+                  disabled={values.timeStart === 0}
+                  options={timeOptions}
+                  value={values.timeEnd === 0 ? "" : values.timeEnd.toString()}
+                  placeholder="Pilih Waktu Selesai"
+                  onChange={onTimeEndChange}
+                />
+              </InputWrapper>
+            </div>
+            <div className="flex items-start gap-5">
+              <InputWrapper
+                label="Jumlah Peserta"
+                labelProps={{ htmlFor: "participant" }}
+                error={formError.participant}
+              >
+                <Input
+                  type="number"
+                  id="participant"
+                  placeholder="Masukkan jumlah peserta"
+                  value={values.participant === 0 ? "" : values.participant.toString()}
+                  min={0}
+                  onChange={onParticipantChange}
+                />
+              </InputWrapper>
+            </div>
+            <div className="flex items-start gap-5">
+              <InputWrapper
+                label="Konsumsi Rapat"
+                labelProps={{ htmlFor: "consumption" }}
+                error={formError.consumption}
+              >
+                <div className="flex flex-col gap-2">
+                  <Checkbox checked={values.consumption["1"]}>Snack Siang</Checkbox>
+                  <Checkbox checked={values.consumption["2"]}>Makan Siang</Checkbox>
+                  <Checkbox checked={values.consumption["3"]}>Snack Sore</Checkbox>
+                </div>
+              </InputWrapper>
+            </div>
+            <div className="flex items-start gap-5">
+              <InputWrapper
+                label="Nominal Konsumsi"
+                labelProps={{ htmlFor: "consumptionPrice" }}
+                error={formError.consumptionPrice}
+              >
+                <Input
+                  type="number"
+                  id="consumptionPrice"
+                  readOnly
+                  placeholder="Masukkan harga konsumsi"
+                  value={values.consumptionPrice.toString()}
+                  extra={{ prefix: "Rp." }}
+                />
+              </InputWrapper>
+            </div>
+          </div>
+
+          <hr className="border-t-2 border-form-border" />
+
+          <div className="flex items-center justify-end gap-2.5">
+            <Link to={"/form"} className={buttonVariants({ variant: "destructive", fill: "ghost" })}>
+              Batal
+            </Link>
+            <Button
+              disabled={
+                Object.values(formError).some((error) => error !== "") ||
+                Object.values(values).some((value) => value === "" || value === 0)
+              }
+              onClick={() => {
+                alert(`Data yang akan disubmit: ${JSON.stringify(values, null, 2)}`);
               }}
-              error={formError.capacity}
             >
-              <Input
-                type="number"
-                id="kapasitas"
-                placeholder="Masukkan kapasitas ruangan"
-                disabled
-                value={values.capacity}
-                onChange={(e) => setValues((prev) => ({ ...prev, capacity: Number(e.target.value) }))}
-              />
-            </InputWrapper>
+              Simpan
+            </Button>
           </div>
         </div>
-
-        <hr className="border-t-2 border-form-border" />
-
-        <div className="flex flex-col gap-5">
-          <h3 className="font-semibold text-base leading-[21.79px]">Informasi Rapat</h3>
-          <div className="flex items-start gap-5">
-            <InputWrapper label="Tanggal Rapat" labelProps={{ htmlFor: "date" }} error={formError.date}>
-              <Input
-                type="date"
-                id="date"
-                placeholder="Pilih tanggal rapat"
-                required
-                value={values.date}
-                onChange={onDateChange}
-              />
-            </InputWrapper>
-            <InputWrapper label="Waktu Mulai" labelProps={{ htmlFor: "timeStart" }} error={formError.timeStart}>
-              <Select
-                id="timeStart"
-                disabled={values.date === ""}
-                options={timeOptions}
-                value={values.timeStart === 0 ? "" : values.timeStart.toString()}
-                placeholder="Pilih Waktu Mulai"
-                onChange={onTimeStartChange}
-              />
-            </InputWrapper>
-            <InputWrapper label="Waktu Selesai" labelProps={{ htmlFor: "timeEnd" }} error={formError.timeEnd}>
-              <Select
-                id="timeEnd"
-                disabled={values.timeStart === 0}
-                options={timeOptions}
-                value={values.timeEnd === 0 ? "" : values.timeEnd.toString()}
-                placeholder="Pilih Waktu Selesai"
-                onChange={onTimeEndChange}
-              />
-            </InputWrapper>
-          </div>
-          <div className="flex items-start gap-5">
-            <InputWrapper label="Jumlah Peserta" labelProps={{ htmlFor: "participant" }} error={formError.participant}>
-              <Input
-                type="number"
-                id="participant"
-                placeholder="Masukkan jumlah peserta"
-                value={values.participant === 0 ? "" : values.participant.toString()}
-                min={0}
-                onChange={onParticipantChange}
-              />
-            </InputWrapper>
-          </div>
-          <div className="flex items-start gap-5">
-            <InputWrapper label="Konsumsi Rapat" labelProps={{ htmlFor: "consumption" }} error={formError.consumption}>
-              <div className="flex flex-col gap-2">
-                <Checkbox checked={values.consumption["1"]}>Snack Siang</Checkbox>
-                <Checkbox checked={values.consumption["2"]}>Makan Siang</Checkbox>
-                <Checkbox checked={values.consumption["3"]}>Snack Sore</Checkbox>
-              </div>
-            </InputWrapper>
-          </div>
-          <div className="flex items-start gap-5">
-            <InputWrapper
-              label="Nominal Konsumsi"
-              labelProps={{ htmlFor: "consumptionPrice" }}
-              error={formError.consumptionPrice}
-            >
-              <Input
-                type="number"
-                id="consumptionPrice"
-                readOnly
-                placeholder="Masukkan harga konsumsi"
-                value={values.consumptionPrice.toString()}
-                extra={{ prefix: "Rp." }}
-              />
-            </InputWrapper>
-          </div>
-        </div>
-
-        <hr className="border-t-2 border-form-border" />
-
-        <div className="flex items-center justify-end gap-2.5">
-          <Link to={"/form"} className={buttonVariants({ variant: "destructive", fill: "ghost" })}>
-            Batal
-          </Link>
-          <Button
-            disabled={
-              Object.values(formError).some((error) => error !== "") ||
-              Object.values(values).some((value) => value === "" || value === 0)
-            }
-            onClick={() => {
-              alert(`Data yang akan disubmit: ${JSON.stringify(values, null, 2)}`);
-            }}
-          >
-            Simpan
-          </Button>
-        </div>
-      </div>
+      )}
     </>
   );
 }
